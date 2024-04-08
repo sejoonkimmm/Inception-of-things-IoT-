6777a4defe6e743bffd0a5d16df87345d776a7c9
sejokim <bod0925@gmail.com>
2024-04-08 18:29:41 +0900

    feat: ubuntu VB에서 삭제하고 다시 설치

--- p1/scripts/controlnode.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644
-
-echo "Waiting for k3s to start..."
-
-cat /var/lib/rancher/k3s/server/node-token > /vagrant/k3s_token
